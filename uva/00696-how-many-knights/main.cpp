303440f9dc1984ef6776003d8a690256bafd5c0d
yuanz4 <32800452+yuanz4@users.noreply.github.com>
2019-08-30 16:58:36 -0500

    696

--- uva/00696-how-many-knights/main.cpp
+++ uva/00696-how-many-knights/main.cpp
@@ -1,5 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
 #include <stdio.h>
 
+using namespace std;
+
+void output(int knights, int row, int col) {
+    printf("%d knights may be placed on a %d row %d column board.\n", knights, row, col);
+}
+
 int main() {
-    printf("Nothing...\n");
+    int row, col;
+    while (cin>>row>>col) {
+        if (!row && !col)
+            break;
+        if (row==1 || col==1)
+            output(max(row,col), row, col);
+        else if (row==2 || col==2) {
+            int l = row==2 ? col : row;
+            output((l/4*2+min(l%4,2))*2, row, col);
+        } else
+            output(ceil((row*col)/2.0), row, col);
+    }
+    return 0;
 }
