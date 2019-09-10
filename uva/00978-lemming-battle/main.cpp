d4d254551cc3c2a730613ed10a17b2f29da8452b
yuanz4 <32800452+yuanz4@users.noreply.github.com>
2019-09-09 21:39:09 -0500

    two problems

--- uva/00978-lemming-battle/main.cpp
+++ uva/00978-lemming-battle/main.cpp
@@ -1,1 +1,59 @@
-// 00978 - Lemming Battle
+#include <iostream>
+#include <set>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int total;
+    cin >> total;
+    for (int i = 0; i < total; i++) {
+    	int B, SG, SB;
+    	cin >> B >> SG >> SB;
+    	multiset<int, greater<int>> g;
+    	multiset<int, greater<int>> b;
+    	int tmp;
+    	for (int j = 0; j < SG; j++) {
+    		cin >> tmp;
+    		g.insert(tmp);
+    	}
+    	for (int j = 0; j < SB; j++) {
+    		cin >> tmp;
+    		b.insert(tmp);
+    	}
+    	while (g.size() && b.size()) {
+    		auto gi = g.begin();
+    		auto bi = b.begin();
+    		vector<int> gs, bs;
+    		for (int j = 0; j < B; j++) {
+    			if (gi!=g.end() && bi!=b.end()) {
+    				if (*gi > *bi)
+    					gs.push_back(*gi-*bi);
+    				else if (*gi < *bi)
+    					bs.push_back(*bi-*gi);
+    				gi++;
+    				bi++;
+    			} else
+    				break;
+    		}
+			g.erase(g.begin(), gi);
+			b.erase(b.begin(), bi);
+			g.insert(gs.begin(), gs.end());
+			b.insert(bs.begin(), bs.end());
+    	}
+    	if (g.empty() && b.empty())
+    		cout << "green and blue died" << endl;
+    	else if (g.empty()) {
+    		cout << "blue wins" << endl;
+    		for (int j: b)
+    			cout << j << endl;
+    	} else {
+    		cout << "green wins" << endl;
+    		for (int j: g)
+    			cout << j << endl;
+    	}
+    	if (i != total-1)
+	    	cout << endl;
+    }
+    return 0;
+}
--- uva/11286-conformity/main.cpp
+++ uva/11286-conformity/main.cpp
@@ -1,1 +1,37 @@
-// 11286 - Conformity
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int size;
+    while (cin >> size) {
+        if (!size)
+        	break;
+        unordered_map<string, int> m;
+        for (int i = 0; i < size; i++) {
+            string s;
+            vector<int> v(5, 0);
+            for (int j = 0; j < 5; j++)
+                cin >> v[j];
+            sort(v.begin(), v.end());
+            for (int j = 0; j < 5; j++)
+                s += to_string(v[j]);
+        	m[s]++;
+        }
+        int max = 0, maxNum = 0;
+        for(auto it = m.begin(); it != m.end(); it++) {
+            if(it->second > max) {
+                max = it->second;
+                maxNum = 0;
+            }
+            if(it->second == max)
+                maxNum += max;
+        }
+        cout << maxNum << endl;
+    }
+    return 0;
+}
