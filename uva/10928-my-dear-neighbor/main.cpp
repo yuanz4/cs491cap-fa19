11ea709693877b007da1f6cff9743efbc3fe4173
yuanz4 <32800452+yuanz4@users.noreply.github.com>
2019-09-12 13:56:21 -0500

    submit

--- uva/10928-my-dear-neighbor/main.cpp
+++ uva/10928-my-dear-neighbor/main.cpp
@@ -1,3 +1,38 @@
-// 10928 - My Dear Neighbors
+#include <cstdio>
+#include <vector>
 
+using namespace std;
 
+int main() {
+	int total;
+	scanf("%d", &total);
+	while (total--) {	
+		int place;
+		scanf("%d\n", &place);
+		char s[10001];
+		vector<int> v(place, 0);
+		int mini = 100000;
+		for (int i = 0; i < place; i++) {
+			fgets(s, 10001, stdin);
+			int cur = 1;
+			for (int j = 0; s[j] != '\n'; j++)
+				if (s[j] == ' ')
+					cur++;
+			v[i] = cur;
+			if (cur < mini)
+				mini = cur;
+		}
+		bool first = true;
+		for (int i = 0; i < place; i++) {
+			if (v[i] == mini) {
+				if (first)
+					first = false;
+				else
+					printf(" ");
+				printf("%d", i+1);
+			}
+		}
+		printf("\n");
+	}
+	return 0;
+}
--- uva/11991-easy-problem-from/main.cpp
+++ uva/11991-easy-problem-from/main.cpp
@@ -1,1 +1,26 @@
-// 11991 - An Easy Problem from Rujia Liu
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n, q;
+    while (cin >> n >> q) {
+	    unordered_map<int, vector<int>> m;
+	    int tmp;
+	    for (int i = 1; i <= n; i++) {
+	    	cin >> tmp;
+	    	m[tmp].push_back(i);
+	    }
+	    int index, num;
+	    while (q--) {
+	    	cin >> index >> num;
+	    	if (m[num].size() < index)
+	    		cout << 0 << endl;
+	    	else
+	    		cout << m[num][index-1] << endl;
+	    }
+    }
+    return 0;
+}
