36d7a9fddd120ff9c7cb4e85c184415ba71abd15
yuanz4 <32800452+yuanz4@users.noreply.github.com>
2019-09-20 18:11:32 -0500

    initial answer

--- uva/11402-ahoy-pirates/main.cpp
+++ uva/11402-ahoy-pirates/main.cpp
@@ -1,3 +1,32 @@
-// UVa 11402 - Ahoy, Pirates
+#include <iostream>
+#include <string>
 
+using namespace std;
 
+int main() {
+	int test;
+	cin >> test;
+	while (test--) {
+		int M;
+		cin >> M;
+		string P;
+		while (M--) {
+			int T;
+			cin >> T;
+			string s;
+			cin >> s;
+			while (T--) {
+				P += s;
+			}
+		}
+		int N = P.size();
+		cin >> M;
+		char Q;
+		int a, b;
+		while (M--) {
+			cin >> Q >> a >> b;
+		}
+		cout << P << endl;
+	}
+	return 0;
+}
