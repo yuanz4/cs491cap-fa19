e62cd6d90faef844f834feeb65e79af0ac5184f4
yuanz4 <32800452+yuanz4@users.noreply.github.com>
2019-09-23 16:14:07 -0500

    7++

--- uva/574-sum-it-up/main.cpp
+++ uva/574-sum-it-up/main.cpp
@@ -1,3 +1,4 @@
-// 574 - Sum it up
+#include <bits/stdc++.h>
 
+using namespace std;
 
--- uva/725-division/main.cpp
+++ uva/725-division/main.cpp
@@ -1,1 +1,32 @@
-// UVa 725 - Division
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int N, n = 0;
+	char s[100];
+	while (scanf("%d", &N), N) {
+		if (n++)
+			printf("\n");
+		int f = 0;
+		for (int fghij = 1234; fghij < 99999; fghij++) {
+			int abcde = fghij * N;
+			sprintf(s, "%05d%05d", abcde, fghij);
+			if (strlen(s) > 10)
+				break;
+			sort(s, s+10);
+			bool b = true;
+			for (int i = 0; i < 10; i++) {
+				if (s[i] != i+'0')
+					b = false;
+			}
+			if (b) {
+				f++;
+				printf("%05d / %05d = %d\n", abcde, fghij, N);
+			}
+		}
+		if(f==0)
+			printf("There are no solutions for %d.\n", N);
+	}
+	return 0;
+}
