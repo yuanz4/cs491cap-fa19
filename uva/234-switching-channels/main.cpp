4cb375568dfe176230835cc9a5421e36ace654d2
yuanz4 <32800452+yuanz4@users.noreply.github.com>
2019-09-23 21:09:30 -0500

    i-12

--- uva/234-switching-channels/main.cpp
+++ uva/234-switching-channels/main.cpp
@@ -1,1 +1,79 @@
-// 234 - Switching Channels
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void checkMiss(int* prog, int* point, int* impor, int* level,
+	int p, int a) {
+	int acc[p];
+	memcpy(acc, prog, p*sizeof(int));
+	for (int i = 1; i < p; i++)
+		acc[i] += acc[i-1];
+
+	for (int i = 0; i < a; i++) {
+		int low = lower_bound(acc, acc+p, point[i]) - acc;
+		int diff;
+		if (low == p) {
+			diff = abs(acc[p-1]-point[i]);
+		} else if (acc[low] == point[i])
+			continue;
+		else if (low == 0)
+			diff = abs(acc[0]-point[i]);
+		else {
+			int a = abs(acc[low-1]-point[i]);
+			int b = abs(acc[low]-point[i]);
+			if (a < b)
+				diff = a;
+			else
+				diff = b;
+		}
+		level[impor[i]] += diff;
+	}
+}
+
+int main() {
+	int p, a;
+	int cnt = 1;
+	while (scanf("%d", &p), p) {
+		int prog[p];
+		for (int i = 0; i < p; i++)
+			scanf("%d", &prog[i]);
+		scanf("%d", &a);
+		int impor[a];
+		int point[a];
+		for (int i = 0; i < a; i++) {
+			scanf("%d", &impor[i]);
+			impor[i] -= 1;
+			scanf("%d", &point[i]);
+		}
+		sort(prog, prog+p);
+		int miss[5];
+		for (int i = 0; i < 5; i++)
+			miss[i] = INT_MAX;
+		int best[p];
+		do {
+			int level[5] = {0};
+			checkMiss(prog, point, impor, level, p, a);
+			bool better = false;
+			for (int i = 0; i < 5; i++) {
+				if (level[i] < miss[i]) {
+					better = true;
+					break;
+				} else if (level[i] > miss[i])
+					break;
+			}
+			if (better) {
+				memcpy(miss, level, 5*sizeof(int));
+				memcpy(best, prog, p*sizeof(int));
+			}
+		} while (next_permutation(prog, prog+p));
+		printf("Data set %d\n", cnt++);
+		printf("  Order:");
+		for (int i = 0; i < p; i++)
+			printf(" %d", best[i]);
+		int total = 0;
+		for (int i = 0; i < 5; i++)
+			total += miss[i];
+		printf("\n  Error: %d\n", total);
+	}
+	return 0;
+}
--- uva/574-sum-it-up/main.cpp
+++ uva/574-sum-it-up/main.cpp
@@ -2,3 +2,45 @@
 
 using namespace std;
 
+int cnt;
+
+void DFS(int* d, vector<int>& v,
+	int index, int acc, int t, int n) {
+	if (acc == t) {
+		cnt++;
+		for (int i = 0; i < v.size(); i++) {
+			if (i)
+				printf("+");
+			printf("%d", v[i]);
+		}
+		printf("\n");
+		return;
+	}
+	if (index>=n || acc>t)
+		return;
+	for (int i = index; i < n; i++) {
+		if (i>index && d[i]==d[i-1])
+			continue;
+		v.push_back(d[i]);
+		acc += d[i];
+		DFS(d, v, i+1, acc, t, n);
+		acc -= d[i];
+		v.pop_back();
+	}
+}
+
+int main() {
+	int t, n;
+	while (scanf("%d %d", &t, &n), t) {
+		printf("Sums of %d:\n", t);
+		int d[n];
+		for (int i = 0; i < n; i++)
+			scanf("%d", &d[i]);
+		vector<int> v;
+		cnt = 0;
+		DFS(d, v, 0, 0, t, n);
+		if (!cnt)
+			printf("NONE\n");
+	}
+	return 0;
+}
