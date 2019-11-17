2f7e0ce456b10256b7e97c7ca1c4ad45dacab188
yuanz4 <32800452+yuanz4@users.noreply.github.com>
2019-11-16 19:26:40 -0600

    rename

--- Problem Sets/5-Combinatorics, Games, Strings/10851 - 2D Hieroglyph Decoder/main.cpp
+++ Problem Sets/5-Combinatorics, Games, Strings/10851 - 2D Hieroglyph Decoder/main.cpp
@@ -1,66 +1,33 @@
 #include <cstdio>
-#include <unordered_map>
-#include <vector>
-#include <string>
 #include <iostream>
+#include <cstring>
 using namespace std;
 
+const int MX = 100;
+
 int main() {
-	int nc, ne, nm;
-	int test = 1;
-	while (scanf("%d", &nc), nc) {
-		unordered_map<string, unordered_map<string, bool>> adj;
-		vector<string> s;
-		string tmp, prev;
-		for (int i = 0; i < nc; i++) {
-			scanf("%d", &ne);
-			for (int j = 0; j < ne; j++) {
-				cin >> tmp;
-				s.push_back(tmp);
-				if (j)
-					adj[prev][tmp] = true;
-				prev = tmp;
+	int test;
+	scanf("%d", &test);
+	while (test--) {
+		char code[10][MX];
+		char ret[MX];
+		for (int i = 0; i < 10; i++)
+			scanf("%s", code[i]);
+		int M = strlen(code[0])-2;
+		ret[M] = '\0';
+		for (int i = 1; i < M+1; i++) {
+			int cur = 0;
+			for (int j = 8; j >= 1; j--) {
+				char tmp = code[j][i];
+				cur *= 2;
+				if (tmp == '/')
+					cur += 0;
+				else
+					cur += 1;
 			}
+			ret[i-1] = static_cast<char>(cur);
 		}
-		int n = s.size();
-		scanf("%d", &nm);
-		for (int i = 0; i < nm; i++) {
-			cin >> prev >> tmp;
-			adj[prev][tmp] = true;
-		}
-		for (string a: s) {
-			for (string b: s) {
-				if (!adj[a].count(b))
-					adj[a][b] = false;
-			}
-		}
-		for (string k: s) {
-			for (string i: s) {
-				for (string j: s) {
-					adj[i][j] = adj[i][j] | (adj[i][k] & adj[k][j]);
-				}
-			}
-		}
-		int ret = 0;
-		vector<vector<string>> v;
-		for (int i = 0; i < n; i++) {
-			for (int j = i+1; j < n; j++) {
-				string a = s[i], b = s[j];
-				if (!adj[a][b] && !adj[b][a]) {
-					ret++;
-					if (v.size() < 2)
-						v.push_back({a, b});
-				}
-			}
-		}
-		if (!ret)
-			printf("Case %d, no concurrent events.\n", test++);
-		else {
-			printf("Case %d, %d concurrent events:\n", test++, ret);
-			for (int i = 0; i < min(ret, 2); i++)
-				printf("(%s,%s) ", v[i][0].c_str(), v[i][1].c_str());
-			printf("\n");
-		}
+		printf("%s\n", ret);
 	}
 	return 0;
 }
